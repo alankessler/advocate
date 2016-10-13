a943060cedb00f70f1f5d9eb053de5b313eb8da5
Prelang Builder <builder@prelang.com>
2016-10-13 03:17:07 +0000

    Generated scaffolding for model 'Community' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :communities
+
   resources :venues
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
